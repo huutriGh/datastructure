2814b7b8d46f5b3a8015496f76e84fce35787bc2
Nguyễn Hữu Trí <huutriqt13@hotmail.com>
2021-10-11 19:57:09 +0700

    Tree continue

--- JAVA/datastructure/src/main/java/algorithms/datastructure/Tree/AbstractTree.java
+++ JAVA/datastructure/src/main/java/algorithms/datastructure/Tree/AbstractTree.java
@@ -23,4 +23,21 @@ public abstract class AbstractTree<E> implements Tree<E> {
         return p == root();
     }
 
+    public int depth(Position<E> p) {
+
+        if (isRoot(p)) {
+            return 0;
+        } else {
+            return 1 + depth(parent(p));
+        }
+    }
+
+    public int height(Position<E> p) {
+        int h = 0;
+        for (Position<E> c : children(p)) {
+            h = Math.max(h, 1 + height(c));
+        }
+        return h;
+    }
+
 }
