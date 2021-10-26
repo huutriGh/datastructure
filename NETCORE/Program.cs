42a4589a2ab3925bdb48614cf7e5d97993a9eba2
Nguyễn Hữu Trí <huutriqt13@hotmail.com>
2021-10-26 18:24:04 +0700

    Regular Expression Matching recusive

--- NETCORE/Program.cs
+++ NETCORE/Program.cs
@@ -6,97 +6,22 @@ namespace netcore
 {
     class Program
     {
-        public static bool isMatch(string s, string p)
+        public static bool IsMatch(string s, string p)
         {
 
-            if (!p.Contains('*') && !p.Contains('.') && s != p)
+            if (string.IsNullOrEmpty(p)) return string.IsNullOrEmpty(s);
+            bool first_match = (!string.IsNullOrEmpty(s) &&
+                                   (p[0] == s[0] || p[0] == '.'));
+
+            if (p.Length >= 2 && p[1] == '*')
             {
-                return false;
-            }
-            else if (p == "*.")
-            {
-                return true;
+                return (IsMatch(s, p.Substring(2)) ||
+                        (first_match && IsMatch(s.Substring(1), p)));
             }
             else
             {
-
-                int i = 0;
-                int j = 0;
-                if (p.StartsWith(".*"))
-                {
-                    j = 2;
-                }
-
-                do
-                {
-                    if (s[i] == p[j])
-                    {
-                        i++;
-                        j++;
-                    }
-                    else if (s[i] != p[j])
-                    {
-                        if ((p[j] != '*' && p[j] != '.') && (p[j + 1] != '*' && p[j + 1] != '.')) /// loi o day
-                        {
-                            return false;
-                        }
-                        else
-                        {
-                            if (p[j] == '.')
-                            {
-                                i++;
-                                j++;
-                            }
-                            else // *
-                            {
-                                var charBeforeMul = ' ';
-                                var tempIndexofI = i - 1;
-                                if (i - 1 < 0)
-                                {
-                                    charBeforeMul = p[i];
-                                    tempIndexofI = 0;
-                                    j++;
-                                }
-
-
-                                for (int z = tempIndexofI; z < s.Length; z++)
-                                {
-                                    if (s[z] != charBeforeMul)
-                                    {
-                                        var charAfterMul = p[++j];
-
-                                        if (s[z] != charAfterMul)
-                                        {
-                                            return false;
-                                        }
-
-                                        else
-                                        {
-                                            i = z + 1;
-
-                                            j++;
-                                            break;
-                                        }
-
-                                    }
-                                    i = z + 1;
-
-
-                                }
-                                j++;
-                            }
-                        }
-                    }
-                } while (i < s.Length || j < p.Length);
-
-
-
-
+                return first_match && IsMatch(s.Substring(1), p.Substring(1));
             }
-
-
-
-            return true;
         }
         static void Main(string[] args)
         {
@@ -105,7 +30,7 @@ namespace netcore
             // Console.WriteLine($"Exam 2: {isMatch("aa", "a*")}");
             // Console.WriteLine($"Exam 3: {isMatch("ab", ".*")}");
             // Console.WriteLine($"Exam 4: {isMatch("aab", "c*a*b")}");
-            Console.WriteLine($"Exam 5: {isMatch("aab", "c*a*b")}");
+            Console.WriteLine($"Exam 5: {IsMatch("aab", "c*a*b")}");
 
 
             Console.WriteLine("Hello World!");
